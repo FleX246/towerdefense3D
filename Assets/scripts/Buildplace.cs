a8239c4f97c106c513b18fe2aa096f714fa5d7f4
FleX246 <alexhoyer2809@gmail.com>
2016-11-24 22:43:50 +0100

    v.1.2
    Version 1.2 siehe Readme

--- Assets/scripts/Buildplace.cs
+++ Assets/scripts/Buildplace.cs
@@ -6,11 +6,17 @@ public class Buildplace : MonoBehaviour {
     // The Tower that should be built
     public GameObject towerPrefab;
 
+    bool bebaut=false;
+
     void OnMouseUpAsButton()
     {
         // Build Tower above Buildplace
-        GameObject g = (GameObject)Instantiate(towerPrefab);
-        g.transform.position = transform.position + Vector3.up;
+        if(!bebaut)
+        {
+            GameObject g = (GameObject)Instantiate(towerPrefab);
+            g.transform.position = transform.position + Vector3.up;
+            bebaut = true;
+        }
     }
 
 
